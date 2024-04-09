b3b35774c605b8b87409d4342be99a5dc9b8e9f7
fish <532798554@qq.com>
2024-04-09 08:07:17 +0800

    统一样式修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,39 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+//element-plus 按需导入
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers:[ElementPlusResolver()]
+    }),
+    Components({
+      //1、配置elementPlus 使用sass 样式配色系统
+      resolvers:[ElementPlusResolver({importStyle:"sass"})]
+    })
   ],
   resolve: {
+    // src @符号实质 路径转换
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        //2、自动导入定制化样式文件进行样式覆盖
+        additionalData:`
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
-})
+});
